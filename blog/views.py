6dd24ed50e37a17ab31b842f2c6f2f94391fe318
Your Name <you@example.com>
2020-09-12 08:31:07 +0900

    privetstvie

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,18 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
+
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('id')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -24,6 +29,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
